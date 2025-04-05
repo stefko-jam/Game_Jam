084add47bba3e50beb49ca0c7ed2458ef70d5b81
Domi <rogi1609@gmail.com>
2025-04-05 14:31:00 +0200

    Create platforms based on hightest positions

--- scenes/generate_platform_test_scene.gd
+++ scenes/generate_platform_test_scene.gd
@@ -1,20 +1,25 @@
 extends Node2D
 
-var hightestPlayerPosition = 0;
+var highestPlayerPosition = 0;
+var highestPlatformPostiion = 0;
+var maxDiff = 20;
+var createPlatform = false;
 
 func _ready():
 	for i in range(10):
 		var x = randi_range(-3, 3)
 		var y = randi_range(-10, 1)
-		var platform = generatePlatform(x * 100, y * 20)
+		var platform = generatePlatform(x * 20, y * 10)
 		add_child(platform)
 	
 func _process(delta: float) -> void:
 	var player = get_node("Player")
-	if (player.position.y < hightestPlayerPosition):
-		print("new position", hightestPlayerPosition)
-		hightestPlayerPosition = player.position.y
-		spawnNewPlatform(hightestPlayerPosition)
+	if (player.position.y < highestPlayerPosition):
+		highestPlayerPosition = player.position.y
+		
+	if (abs(highestPlatformPostiion) - abs(highestPlayerPosition) < maxDiff):
+		print("new position", highestPlayerPosition)
+		spawnNewPlatform(highestPlayerPosition)
 	
 func generatePlatform(x: int, y: int) -> Node:
 	var PlatformScene = preload("res://scenes/platform.tscn")
@@ -23,6 +28,7 @@ func generatePlatform(x: int, y: int) -> Node:
 	return platform
 
 func spawnNewPlatform(position):
-	var x = randi_range(-3, 3) * 10
-	var y = hightestPlayerPosition - randi_range(0, 10) * 10
+	var x = randi_range(-3, 3) * 5
+	var y = highestPlayerPosition - randi_range(2, 10) * 10
+	highestPlatformPostiion = y;
 	add_child(generatePlatform(x, y))
