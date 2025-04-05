ee6d46e3103ff8cdfdbd160880d7d8c0a0be142d
Domi <rogi1609@gmail.com>
2025-04-05 12:27:30 +0200

    Generate multiple platforms

--- scenes/generate_platform_test_scene.gd
+++ scenes/generate_platform_test_scene.gd
@@ -1,8 +1,16 @@
 extends Node2D
 
 func _ready():
+	for i in range(10):
+		var x = randi_range(-3, 3)
+		var y = randi_range(-10, 1)
+		var platform = generatePlatform(x * 100, y * 20)
+		add_child(platform)
 	
+	
+func generatePlatform(x: int, y: int) -> Node:
 	var PlatformScene = preload("res://scenes/platform.tscn")
 	var platform = PlatformScene.instantiate()
-	platform.position = Vector2(100, 100)
-	add_child(platform)
+	platform.position = Vector2(x, y)
+	return platform
+	
