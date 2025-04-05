321f5c0abfefbfe73e36e5e7317aa03227b53c91
stefko-jam <kogler.stefanie@gmail.com>
2025-04-05 14:37:00 +0200

    add boundary
    commit

--- scripts/camera_2d.gd
+++ scripts/camera_2d.gd
@@ -7,7 +7,7 @@ func _ready():
 	highest_y = global_position.y
 	fixed_x = global_position.x  # Lock the initial horizontal position
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if get_parent().global_position.y < highest_y:
 		highest_y = get_parent().global_position.y
 
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,15 +3,6 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
 
-@onready var camera: Camera2D = $Camera2D
-
-var screen_half_width := 0.0
-
-func _ready():
-	await get_tree().process_frame  # Wait for viewport to be ready
-
-	# Get actual screen width in world units (important!)
-	screen_half_width = get_viewport().get_visible_rect().size.x * camera.zoom.x * 0.5
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -22,7 +13,8 @@ func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Handle horizontal movement
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -30,17 +22,3 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-
-	handle_horizontal_wrap()
-
-
-func handle_horizontal_wrap():
-	var cam_x = camera.global_position.x
-	var player_x = global_position.x
-	var left_edge = cam_x - screen_half_width
-	var right_edge = cam_x + screen_half_width
-
-	if player_x < left_edge:
-		global_position.x = right_edge
-	elif player_x > right_edge:
-		global_position.x = left_edge
