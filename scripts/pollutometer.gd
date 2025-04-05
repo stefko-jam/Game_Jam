04974240a8eaa05fbc2f53bec2104235b6f07d3d
stefko <kogler.stefanie@gmail.com>
2025-04-06 01:04:12 +0200

    working

--- scripts/pollutometer.gd
+++ scripts/pollutometer.gd
@@ -3,8 +3,14 @@ extends Node2D
 @export var frame: int
 
 func _ready():
+	update_position()
 	$AnimatedSprite2D.frame = frame
 
 func _process(delta: float) -> void:
+	update_position()
 	$AnimatedSprite2D.frame = frame
-	#position.y -= delta
+
+func update_position():
+	var viewport_size = get_viewport_rect().size
+	position.y = 0  # Top of the screen
+	position.x = viewport_size.x / 2  # Optional: center it horizontally
