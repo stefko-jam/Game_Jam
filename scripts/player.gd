c2801aa843c784583bed6ea9d7dbc849d1cc86b1
stefko <kogler.stefanie@gmail.com>
2025-04-05 18:17:11 +0200

    Frog animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,20 +2,30 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
-#@onready var game_over_ui: CanvasLayer = $"../GameOverUI"
 
+@onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump_timer: Timer = $JumpTimer
+
+var pending_jump := false
+var was_on_floor := false
+
+func _ready():
+	jump_timer.timeout.connect(_on_jump_timer_timeout)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		anim_sprite.play("jump")
 
-	# Handle jump.
-	if is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# Detect fresh landing
+	if is_on_floor() and not was_on_floor and !pending_jump:
+		anim_sprite.play("on floor")
+		print("on floor")
+		jump_timer.start()
+		pending_jump = true
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	was_on_floor = is_on_floor()
+
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -23,4 +33,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-		
+
+
+func _on_jump_timer_timeout():
+	velocity.y = JUMP_VELOCITY
+	pending_jump = false
