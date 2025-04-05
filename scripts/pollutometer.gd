dbec460c674e1397bf4a4d6bd8dfaae82e37b7ff
stefko <kogler.stefanie@gmail.com>
2025-04-06 01:07:25 +0200

    Health bar working

--- scripts/pollutometer.gd
+++ scripts/pollutometer.gd
@@ -3,14 +3,8 @@ extends Node2D
 @export var frame: int
 
 func _ready():
-	update_position()
 	$AnimatedSprite2D.frame = frame
 
 func _process(delta: float) -> void:
-	update_position()
 	$AnimatedSprite2D.frame = frame
-
-func update_position():
-	var viewport_size = get_viewport_rect().size
-	position.y = 0  # Top of the screen
-	position.x = viewport_size.x / 2  # Optional: center it horizontally
+	#position.y -= delta
