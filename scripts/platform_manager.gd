47d12c4374ae455bc311fd71d9603a0b752ae3fe
Domi <rogi1609@gmail.com>
2025-04-05 15:53:40 +0200

    Refactor patform manager

--- scripts/platform_manager.gd
+++ scripts/platform_manager.gd
@@ -1,34 +1,41 @@
 extends Node2D
 
 var highestPlayerPosition = 0;
-var highestPlatformPostiion = 0;
-var maxDiff = 20;
+var highestPlatformPostiion = 200;
+var maxDiff = 200;
 var createPlatform = false;
 
+# LIFECYCLE HOOKS
 func _ready():
-	for i in range(10):
-		var x = randi_range(-3, 3)
-		var y = randi_range(-10, 1)
-		var platform = generatePlatform(x * 20, y * 10)
-		add_child(platform)
-	
+	initializePlatforms()
+
 func _process(delta: float) -> void:
+	updateHighestPlayerPosition()
+	if shouldGeneratePlatform():
+		spawnNewPlatform()
+
+# CUSTOM METHODS
+func initializePlatforms():
+	for i in range(10):
+		spawnNewPlatform()
+
+func updateHighestPlayerPosition():
 	var player = get_node("Player")
 	if (player.position.y < highestPlayerPosition):
 		highestPlayerPosition = player.position.y
-		
-	if (abs(highestPlatformPostiion) - abs(highestPlayerPosition) < maxDiff):
-		print("new position", highestPlayerPosition)
-		spawnNewPlatform(highestPlayerPosition)
+
+func shouldGeneratePlatform():
+	return abs(highestPlatformPostiion) - abs(highestPlayerPosition) < maxDiff
 	
 func generatePlatform(x: int, y: int) -> Node:
+	print()
 	var PlatformScene = preload("res://scenes/platform.tscn")
 	var platform = PlatformScene.instantiate()
 	platform.position = Vector2(x, y)
 	return platform
 
-func spawnNewPlatform(position):
-	var x = randi_range(-3, 3) * 5
-	var y = highestPlayerPosition - randi_range(2, 10) * 10
+func spawnNewPlatform():
+	var x = randi_range(-100, 100)
+	var y = highestPlatformPostiion - randi_range(2, 10) * 10
 	highestPlatformPostiion = y;
 	add_child(generatePlatform(x, y))
