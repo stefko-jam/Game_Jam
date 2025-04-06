dd4ca1872c6840c4fc993112966fa9728fb1e578
Domi <rogi1609@gmail.com>
2025-04-06 23:40:09 +0200

    Support mobile touch input

--- scripts/player.gd
+++ scripts/player.gd
@@ -29,8 +29,9 @@ func _physics_process(delta: float) -> void:
 	if input_enabled:
 			
 		was_on_floor = is_on_floor()
-
-		var direction := Input.get_axis("left", "right")
+		
+		var keyboardInput = Input.get_axis("left", "right");
+		var direction = touchInput if touchInput != 0 else keyboardInput
 		if direction:
 			velocity.x = direction * SPEED
 		else:
@@ -45,3 +46,13 @@ func _on_jump_timer_timeout():
 
 func enable_input():
 	input_enabled = true
+
+var touchInput := 0  # -1 for left, 1 for right, 0 for none
+
+func _unhandled_input(event):
+	if event is InputEventScreenTouch or event is InputEventScreenDrag:
+		var screen_half = get_viewport_rect().size.x / 2
+		touchInput = -1 if event.position.x < screen_half else 1
+			
+	if event is InputEventScreenTouch and not event.pressed:
+		touchInput = 0 # Stop moving on release
